38830493a4e68bd308b223f398c40efd17786038
Gaurav Kumar <gauravkumar803109@gmail.com>
2025-12-09 16:57:02 +0530

    complete setup of shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { ThemeToggle } from '@/components/ui/theme-toggle'
 
 const page = () => {
   return (
     <div>
       <h1 className='text-center mt-12'>Hello World!</h1>
+      <ThemeToggle />
     </div>
   )
 }
