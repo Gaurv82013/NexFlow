e7b37e300ceab5f528a5e9aff50474b4b477b764
Gaurav Kumar <gauravkumar803109@gmail.com>
2025-12-09 18:46:06 +0530

    Create HomePage using tailark

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+// import { ThemeToggle } from '@/components/ui/theme-toggle'
+import {HeroHeader} from '@/app/(marketing)/_components/header'
+import HeroSection from '@/app/(marketing)/_components/hero-section'
+
+export default function Page() {
+  return (
+    <div>
+
+      <HeroHeader />
+      <HeroSection />
+    </div>
+  )
+}
